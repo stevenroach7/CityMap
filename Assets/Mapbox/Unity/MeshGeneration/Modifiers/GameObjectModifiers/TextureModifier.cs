6e122b916534525c08dc26a8e9f593d7da54b39f
stevenroach7 <sroach@macalester.edu>
2017-11-14 19:34:10 -0600

    Removing color blotching on non city area

--- Assets/Mapbox/Unity/MeshGeneration/Modifiers/GameObjectModifiers/TextureModifier.cs
+++ Assets/Mapbox/Unity/MeshGeneration/Modifiers/GameObjectModifiers/TextureModifier.cs
@@ -33,6 +33,9 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
 		[SerializeField]
 		private float _maxDataValue = 235;
 
+		[SerializeField]
+		private string _cityString = "";
+
 
 
 		public override void Run(FeatureBehaviour fb, UnityTile tile)
@@ -42,13 +45,19 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
 			{
 				float dataValue = _minDataValue;
 				Material sideMaterial = _sideMaterials[Random.Range (0, _sideMaterials.Length)];
-				if (fb.Data.Properties.ContainsKey(_sideColorDataKey)) {
-					if (float.TryParse(fb.Data.Properties[_sideColorDataKey].ToString (), out dataValue)) 
-					{
-						float colorPercent = (dataValue - _minDataValue) / (_maxDataValue - _minDataValue);
-						Color sideMaterialColor = Color.Lerp (Color.white, Color.blue, colorPercent);
-						sideMaterial.SetColor("_Color", sideMaterialColor);
+				if ((fb.Data.Properties.ContainsKey ("City")) && (fb.Data.Properties ["City"].Equals (_cityString))) 
+				{
+					if (fb.Data.Properties.ContainsKey (_sideColorDataKey)) {
+						if (float.TryParse (fb.Data.Properties [_sideColorDataKey].ToString (), out dataValue)) 
+						{
+							float colorPercent = (dataValue - _minDataValue) / (_maxDataValue - _minDataValue);
+							Color sideMaterialColor = Color.Lerp (Color.white, Color.blue, colorPercent);
+							sideMaterial.SetColor ("_Color", sideMaterialColor);
+						}
 					}
+				} else 
+				{
+					sideMaterial.SetColor("_Color", Color.clear);
 				}
 				_meshRenderer.materials = new Material[2]
 				{
